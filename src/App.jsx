0636edbc160a514716374b4f30980e7c371a6ea1
NagiMuseibli <nagimuseibli02@gmail.com>
2024-01-06 23:50:45 +0400

    Adding react files

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import './App.css'
+import SearchHeader from './searchHeader'
+
+function App() {
+
+  return (
+    <div className='App'>
+      <SearchHeader />
+    </div>
+
+  )
+}
+
+export default App
---
+++ src/SearchHeader.jsx
@@ -0,0 +1,12 @@
+function SearchHeader() {
+    return (
+        <div className="searchDiv">
+            <form>
+                <label>Ne axtarirsan??</label>
+                <input />
+            </form>
+        </div>
+    );
+}
+
+export default SearchHeader;
